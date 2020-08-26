cb181297a7b4ce2cae956980b0bb4e7ddbc46f05
Alan Nascimento <alan.silva.n@hotmail.com>
2020-08-26 01:33:58 -0300

    refactor: remove validation properties

--- src/presentation/protocols/validation.ts
+++ src/presentation/protocols/validation.ts
@@ -1,7 +1,5 @@
 export interface Validation {
   errorMessage: string
-  fieldName: string
-  fieldValue: string
 
   validate (fieldName: string, fieldValue: string): string
 }
--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -2,13 +2,8 @@ import { Validation } from '@/presentation/protocols'
 
 export class ValidationStub implements Validation {
   errorMessage: string
-  fieldName: string
-  fieldValue: string
 
   validate (fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
-
     return this.errorMessage
   }
 }
