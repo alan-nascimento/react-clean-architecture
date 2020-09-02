20ae88140a0c83093f1f7dd7a9c5025fff911f21
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-02 18:17:38 -0300

    test: ensure login navigates to signup route

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,6 +2,8 @@ import 'jest-localstorage-mock'
 
 import React from 'react'
 import faker from 'faker'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
 import { render, RenderResult, fireEvent, cleanup, waitFor } from '@testing-library/react'
 
 import { Authentication } from '@/domain/usecases'
@@ -19,13 +21,19 @@ type SutParams = {
   validationError: string
 }
 
+const history = createMemoryHistory()
+
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
   const authenticationSpy = new AuthenticationSpy()
 
   validationStub.errorMessage = params?.validationError
 
-  const sut = render(<Login validation={validationStub} authentication={authenticationSpy} />)
+  const sut = render(
+    <Router history={history}>
+      <Login validation={validationStub} authentication={authenticationSpy} />
+    </Router>
+  )
 
   return {
     sut,
@@ -191,5 +199,15 @@ describe('Login Component', () => {
     await waitFor(() => sut.getByTestId('login-form'))
 
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken)
+  })
+
+  it('should go to signup page', () => {
+    const { sut } = makeSut()
+
+    const signup = sut.getByTestId('signup')
+    fireEvent.click(signup)
+
+    expect(history.length).toBe(2)
+    expect(history.location.pathname).toBe('/signup')
   })
 })
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 import { Validation } from '@/presentation/protocols'
 import { FormContext } from '@/presentation/contexts'
@@ -67,7 +68,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
             Sign In
           </button>
 
-          <span className={Styles.link}>Create an account</span>
+          <Link to="/signup" data-testid="signup" className={Styles.link}>Create an account</Link>
 
           <FormStatus />
         </form>
