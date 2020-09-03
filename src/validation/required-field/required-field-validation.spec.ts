eb6837832163ff2a1f7afedc76374799d66bc576
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-03 18:52:04 -0300

    feat: ensure RequiredFieldValidation returns falsy if field is not emptty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,12 +1,33 @@
+import faker from 'faker'
+
 import { RequiredFieldError } from '@/validation/errors'
 
 import { RequiredFieldValidation } from './required-field-validation'
 
+type SutTypes = {
+  sut: RequiredFieldValidation
+}
+
+const makeSut = (field: string): SutTypes => {
+  const sut = new RequiredFieldValidation(field)
+
+  return {
+    sut
+  }
+}
+
 describe('RequiredFieldValidation', () => {
   it('should return an error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const { sut } = makeSut('email')
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('should return falsy if field is not empty', () => {
+    const { sut } = makeSut('email')
+    const error = sut.validate(faker.random.words())
+
+    expect(error).toBeFalsy()
   })
 })
