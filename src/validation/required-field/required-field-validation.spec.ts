393ab8721c20fecdc4d0e6a5d6968210d9c738b2
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-03 18:56:47 -0300

    refactor: insert a fake value to the sut required vaidation test

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -8,8 +8,8 @@ type SutTypes = {
   sut: RequiredFieldValidation
 }
 
-const makeSut = (field: string): SutTypes => {
-  const sut = new RequiredFieldValidation(field)
+const makeSut = (): SutTypes => {
+  const sut = new RequiredFieldValidation(faker.database.column())
 
   return {
     sut
@@ -18,14 +18,14 @@ const makeSut = (field: string): SutTypes => {
 
 describe('RequiredFieldValidation', () => {
   it('should return an error if field is empty', () => {
-    const { sut } = makeSut('email')
+    const { sut } = makeSut()
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
   })
 
   it('should return falsy if field is not empty', () => {
-    const { sut } = makeSut('email')
+    const { sut } = makeSut()
     const error = sut.validate(faker.random.words())
 
     expect(error).toBeFalsy()
