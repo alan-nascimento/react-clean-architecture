26df2f22fb0afba7ff389f654dd47bcb02cb21db
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-07 15:51:06 -0300

    refactor: move sut creation to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -2,27 +2,32 @@ import { FieldValidationSpy } from '@/validation/validators/test'
 
 import { ValidationComposite } from './validation-composite'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+
+  const sut = new ValidationComposite(fieldValidationsSpy)
+
+  return {
+    sut,
+    fieldValidationsSpy
+  }
+}
+
 describe('ValidationComposite', () => {
   it('should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    const { sut, fieldValidationsSpy } = makeSut()
 
-    fieldValidationSpy2.error = new Error('any_error_message')
+    fieldValidationsSpy[0].error = new Error('first_error_message')
+    fieldValidationsSpy[1].error = new Error('second_error_message')
 
-    const sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpy2])
-    const error = sut.validate('any_field', 'any_value')
-
-    expect(error).toBe('any_error_message')
-  })
-
-  it('should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    fieldValidationSpy.error = new Error('first_error_message')
-
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('second_error_message')
-
-    const sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpy2])
     const error = sut.validate('any_field', 'any_value')
 
     expect(error).toBe('first_error_message')
