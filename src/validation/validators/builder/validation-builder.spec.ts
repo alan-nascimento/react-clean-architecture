827ef29a86c01258cfd740c926a6956c02fb3ca0
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-07 23:10:17 -0300

    feat: ensure ValidationBuilder has a nemail helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
 
 import { ValidationBuilder as sut } from './validation-builder'
 
@@ -7,5 +7,11 @@ describe('ValidationBuilder', () => {
     const validations = sut.field('any_field').required().build()
 
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  it('should return RequiredFieldValidation', () => {
+    const validations = sut.field('any_field').email().build()
+
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
