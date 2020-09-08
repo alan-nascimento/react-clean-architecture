12e580a9dcc2c472429388359fab7ef07d4dc507
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-08 00:48:52 -0300

    fix: change axios http client to handle exceptiions

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { mockAxios } from '@/infra/test'
+import { mockAxios, mockHttpResponse } from '@/infra/test'
 import { mockPostRequest } from '@/data/test'
 
 import { AxiosHttpClient } from './axios-http-client'
@@ -34,6 +34,18 @@ describe('AxiosHttpClient', () => {
 
   it('should return the correct statusCode and body', () => {
     const { sut, mockedAxios } = makeSut()
+
+    const promise = sut.post(mockPostRequest())
+
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+  })
+
+  it('should return the correct statusCode and body on failure', () => {
+    const { sut, mockedAxios } = makeSut()
+
+    mockedAxios.post.mockRejectedValueOnce({
+      response: mockHttpResponse()
+    })
 
     const promise = sut.post(mockPostRequest())
 
--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,10 +1,16 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 import { HttpPostParams, HttpResponse, HttpPostClient } from '@/data/protocols/http'
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body)
+    let httpResponse: AxiosResponse<any>
+
+    try {
+      httpResponse = await axios.post(params.url, params.body)
+    } catch (err) {
+      httpResponse = err.response
+    }
 
     return {
       statusCode: httpResponse.status,
