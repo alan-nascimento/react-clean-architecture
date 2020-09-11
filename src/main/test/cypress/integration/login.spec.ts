c0558996e89e8ba6ce9a6b39071b7b7bb94396a6
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-11 13:23:04 -0300

    chore: add cypress

---
+++ src/main/test/cypress/integration/login.spec.ts
@@ -0,0 +1,5 @@
+describe('Login', () => {
+  it('should load with correct initial state', () => {
+    cy.visit('login')
+  })
+})
---
+++ src/main/test/cypress/plugins/cy-ts-preprocessor.js
@@ -0,0 +1,18 @@
+const wp = require('@cypress/webpack-preprocessor')
+
+module.exports = wp({
+  webpackOptions: {
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          loader: 'ts-loader'
+        }
+      ]
+    }
+  }
+})
