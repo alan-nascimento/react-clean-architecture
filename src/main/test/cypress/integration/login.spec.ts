5158349debf6daf0263914f3e6c0ffe7dfcc4e64
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-11 14:28:58 -0300

    test: ensure Login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,15 +1,37 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
 
   it('should load with correct initial state', () => {
+    cy.getByTestId('email-input').should('have.attr', 'readOnly')
+    cy.getByTestId('password-input').should('have.attr', 'readOnly')
+
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Required field')
       .should('contain.text', '🔴')
 
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Required field')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('submit-button').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('should present error if form is invalid', () => {
+    cy.getByTestId('email-input').focus().type(faker.random.word())
+
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Invalid field')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('password-input').focus().type(faker.random.alphaNumeric(4))
+
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Invalid field')
       .should('contain.text', '🔴')
 
     cy.getByTestId('submit-button').should('have.attr', 'disabled')
