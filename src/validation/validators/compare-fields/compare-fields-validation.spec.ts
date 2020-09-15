9b43e5bf791e1614b7adb0105026f361cbea07ad
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-15 13:55:54 -0300

    fix: change compare fields validation test to never compare equal values

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -23,8 +23,8 @@ describe('CompareFieldsValidation', () => {
 
     const { sut } = makeSut(field, faker.random.word())
     const error = sut.validate({
-      [field]: faker.random.word(),
-      [fieldToCompare]: faker.random.word()
+      [field]: faker.random.words(3),
+      [fieldToCompare]: faker.random.words(4)
     })
 
     expect(error).toEqual(new InvalidFieldError())
