3f0633f03fe308c55cd38c809f4dbae9306d5d5a
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-16 11:15:04 -0300

    refactor: move history to helper

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 
 import PrivateRoute from './private-route'
 
+type SutTypes = {
+  history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+
+  render(
+    <Router history={history}>
+      <PrivateRoute />
+    </Router>
+  )
+
+  return {
+    history
+  }
+}
+
 describe('PrivateRoute', () => {
   it('should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-
-    render(
-      <Router history={history}>
-        <PrivateRoute />
-      </Router>
-    )
+    const { history } = makeSut()
 
     expect(history.location.pathname).toBe('/login')
   })
