5abb6d37ae3d7bd60efd3ee226951aeb2339b4e6
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-16 09:52:12 -0300

    test: ensure CurrentAccountAdapater throws

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -2,6 +2,7 @@ import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 
 import { setCurrentAccountAdapter } from './current-account-adapter'
+import { UnexpectedError } from '@/domain/errors'
 
 jest.mock('@/infra/cache/local-storage-adapter')
 
@@ -14,5 +15,11 @@ describe('CurrentAccountAdapter', () => {
     setCurrentAccountAdapter(account)
 
     expect(setSpy).toHaveBeenCalledWith('account', account)
+  })
+
+  it('should throw UnexpectedError', () => {
+    expect(() => {
+      setCurrentAccountAdapter(undefined)
+    }).toThrow(new UnexpectedError())
   })
 })
--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -3,7 +3,7 @@ import { UnexpectedError } from '@/domain/errors'
 import { makeLocalStorageAdapter } from '@/main/factories/cache/local-storage-adapter-factory'
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account.accessToken) {
+  if (!account?.accessToken) {
     throw new UnexpectedError()
   }
 
