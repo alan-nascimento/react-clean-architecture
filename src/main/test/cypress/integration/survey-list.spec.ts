ad0b885b1b01810c34c96c227638fac4a859d13c
Alan Nascimento <alan.silva.n@hotmail.com>
2020-09-30 13:07:21 -0300

    test: ensure SurveyList logout on logout link click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -30,4 +30,13 @@ describe('SurveyList', () => {
     cy.visit('')
     cy.getByTestId('username').should('contain.text', name)
   })
+
+  it('should logout on logout link click', () => {
+    Http.mockUnexpectedError()
+
+    cy.visit('')
+    cy.getByTestId('logout').click()
+
+    Helper.testUrl('/login')
+  })
 })
