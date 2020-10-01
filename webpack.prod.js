97221b7bef4908568df49ed311ba006aeb960d91
Alan Nascimento <alan.silva.n@hotmail.com>
2020-10-01 19:31:46 -0300

    chore: add recoil

--- webpack.prod.js
+++ webpack.prod.js
@@ -35,6 +35,7 @@ module.exports = merge(common, {
   externals: {
     react: 'React',
     axios: 'axios',
+    recoil: 'Recoil',
     'react-dom': 'ReactDOM',
     'react-router-dom': 'ReactRouterDOM'
   },
