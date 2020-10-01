e14d2603f465737e1a3f9393b3fe9e2a0c5211b5
Alan Nascimento <alan.silva.n@hotmail.com>
2020-10-01 11:41:28 -0300

    test: ensure RemoteLoadSurveyRResult throws on 400 and 500

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,6 +1,8 @@
 import faker from 'faker'
 
+import { HttpStatusCode } from '@/data/protocols'
 import { HttpGetClientSpy } from '@/data/test'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 
 import { RemoteLoadSurveyResult } from './remote-load-survey-result'
 
@@ -27,5 +29,41 @@ describe('RemoteLoadSurveyResult', () => {
     await sut.load()
 
     expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  it('should throw AccessDeniedError if HttpGetClient returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+
+    const promise = sut.load()
+
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
+  })
+
+  it('should throw UnexpectedError if HttpGetClient returns 404', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+
+    const promise = sut.load()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should throw UnexpectedError if HttpGetClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+
+    const promise = sut.load()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,5 +1,6 @@
-import { HttpGetClient } from '@/data/protocols'
 import { LoadSurveyResult } from '@/domain/usecases'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols'
 
 export class RemoteLoadSurveyResult implements LoadSurveyResult {
   constructor (
@@ -8,8 +9,15 @@ export class RemoteLoadSurveyResult implements LoadSurveyResult {
   ) {}
 
   async load (): Promise<LoadSurveyResult.Model> {
-    await this.httpGetClient.get({ url: this.url })
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
 
-    return null
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return null
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError()
+      default:
+        throw new UnexpectedError()
+    }
   }
 }
